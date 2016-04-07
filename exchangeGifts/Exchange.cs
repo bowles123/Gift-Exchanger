a28d71e0d9d736e7e5066577f37c27a6b543a0be
Brian <bowles123@gmail.com>
2016-04-07 12:50:58 -0600

    Added Thompson family functions

--- exchangeGifts/Exchange.cs
+++ exchangeGifts/Exchange.cs
@@ -13,6 +13,134 @@ namespace exchangeGifts
             while (!finished)
                 finished = BowlesFamily();
         }
+
+        // This function does the exchange gifts for the Thompson family.
+        static bool individualThompsonFamily()
+        {
+            // Variables.
+            List<string> individualNames1 = new List<string>(), individualNames2 = new List<string>();
+            Dictionary<string, string> lastYear = new Dictionary<string, string>();
+            System.IO.StreamWriter outFile = new System.IO.StreamWriter(@"individualThompsonThisYear.txt");
+            int individualParticipants = 0, individualDone = 0;
+            string name, one, two, last;
+
+            // Get the assignments from the previous year.
+            lastYear = readFromFile("individualThompsonLastYear.txt");
+
+            // Get the name of the first participant for the individual exchange gifts.
+            Console.Write("Please enter participants name for individual trade: ");
+            name = Console.ReadLine();
+            individualNames1.Add(name);
+            individualNames2.Add(name);
+            individualDone++;
+
+            while (individualParticipants > individualDone)
+            {
+                // Keep getting the names of the participants for the indiviudal exchange gifst
+                // until all of them have been entered.
+                Console.Write("Please enter participants name for individual trade: ");
+                name = Console.ReadLine();
+                individualNames1.Add(name);
+                individualNames2.Add(name);
+                individualDone++;
+            }
+            Shuffle(individualNames1); // Shuffle the names so the list is randomized.
+
+            // Assign people to names until everyone has been assigned.
+            do
+            {
+                // Get the two names.
+                one = individualNames1[0];
+                individualNames1.Remove(one);
+                two = individualNames2[0];
+                individualNames2.Remove(two);
+                bool there = lastYear.TryGetValue(one, out last);
+
+                // If the names are the same or the first gave to the second last year retry the assignment.
+                while (one == two || (there == true && two == last))
+                {
+                    // Add the names back, shuffle the second list, and try again.
+                    individualNames2.Add(two);
+                    two = individualNames2[0];
+                    individualNames2.Remove(two);
+                    there = lastYear.TryGetValue(one, out last);
+                }
+
+                // Output the assignments to the console and to a file.
+                Console.WriteLine("{0} -> {1}.", one, two);
+                outFile.WriteLine(String.Format("{0},{1}", one, two));
+                individualDone++;
+            }
+            while (individualNames1.Count > 0 && individualDone <= individualParticipants);
+
+            if (individualNames1.Count > 0) return false;
+            outFile.Close();
+            return true;
+        }
+
+        static bool familyThompsonFamily()
+        {
+            List<string> familyNames1 = new List<string>(), familyNames2 = new List<string>();
+            Dictionary<string, string> lastYear = new Dictionary<string, string>();
+            System.IO.StreamWriter outFile = new System.IO.StreamWriter(@"familyThompsonThisYear.txt");
+            int familyParticipants = 0, familyDone = 0;
+            string name, one, two, last;
+
+            // Get the assignments from the previous year.
+            lastYear = readFromFile("familyThompsonLastYear.txt");
+
+            // Get the name of the first participant for the individual exchange gifts.
+            Console.Write("Please enter participants name for family trade: ");
+            name = Console.ReadLine();
+            familyNames1.Add(name);
+            familyNames2.Add(name);
+            familyDone++;
+
+            while (familyParticipants > familyDone)
+            {
+                // Keep getting the names of the participants for the indiviudal exchange gifst
+                // until all of them have been entered.
+                Console.Write("Please enter participants name for family trade: ");
+                name = Console.ReadLine();
+                familyNames1.Add(name);
+                familyNames2.Add(name);
+                familyParticipants++;
+            }
+            Shuffle(familyNames1); // Shuffle the names so the list is randomized.
+
+            // Assign people to names until everyone has been assigned.
+            do
+            {
+                // Get the two names.
+                one = familyNames1[0];
+                familyNames1.Remove(one);
+                two = familyNames2[0];
+                familyNames2.Remove(two);
+                bool there = lastYear.TryGetValue(one, out last);
+
+                // If the names are the same or the first gave to the second last year retry the assignment.
+                while (one == two || (there == true && two == last))
+                {
+                    // Add the names back, shuffle the second list, and try again.
+                    familyNames2.Add(two);
+                    two = familyNames2[0];
+                    familyNames2.Remove(two);
+                    there = lastYear.TryGetValue(one, out last);
+                }
+
+                // Output the assignments to the console and to a file.
+                Console.WriteLine("{0} -> {1}.", one, two);
+                outFile.WriteLine(String.Format("{0},{1}", one, two));
+                familyDone++;
+            }
+            while (familyNames1.Count > 0 && familyDone <= familyParticipants);
+
+            if (familyNames1.Count > 0) return false;
+            outFile.Close();
+            return true;
+        }
+
+        // This function does the exchange gifts for the Bowles family.
         static bool BowlesFamily()
         {
             // Variables.
@@ -23,7 +151,7 @@ namespace exchangeGifts
             string name, one, two, last;
 
             // Get the number of particpants for the year.
-            lastYear = readFromFile(); // Get the assignments from the previous year.
+            lastYear = readFromFile("lastYear.txt"); // Get the assignments from the previous year.
             Console.Write("How many are participating this year? ");
             participants = int.Parse(Console.ReadLine());
 
@@ -106,11 +234,11 @@ namespace exchangeGifts
 
         // This function reads from the "lastYear.txt" file 
         // and returns a dictionary of who was assigned to whom the previous year.
-        static Dictionary<string, string> readFromFile()
+        static Dictionary<string, string> readFromFile(string fileName)
         {
             // Variables.
             Dictionary<string, string> lastTime = new Dictionary<string, string>();
-            String[] lines = File.ReadAllLines("lastYear.txt");
+            String[] lines = File.ReadAllLines(fileName);
             string[] names;
             string one, two;
 
