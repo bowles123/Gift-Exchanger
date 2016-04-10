774622b7c84a1e9f53ff61ce0e82ca98c10b49af
Brian <bowles123@gmail.com>
2016-04-09 20:19:50 -0600

    GUI components for exchange gifts

---
+++ exchangeGiftsGUI/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace exchangeGiftsGUI
+{
+    partial class ExchangeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BowlesButton = new System.Windows.Forms.Button();
+            this.ThompsonButton = new System.Windows.Forms.Button();
+            this.Exchanges = new System.Windows.Forms.TextBox();
+            this.EntryLabel = new System.Windows.Forms.Label();
+            this.EntryTextBox = new System.Windows.Forms.TextBox();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.ExchangeLabel = new System.Windows.Forms.Button();
+            this.FamilyExchangeTextBox = new System.Windows.Forms.TextBox();
+            this.FamilyEnterLabel = new System.Windows.Forms.Label();
+            this.FamilyEntryTextBox = new System.Windows.Forms.TextBox();
+            this.FamilyEnterButton = new System.Windows.Forms.Button();
+            this.FamilyExchangeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BowlesButton
+            // 
+            this.BowlesButton.Location = new System.Drawing.Point(13, 13);
+            this.BowlesButton.Name = "BowlesButton";
+            this.BowlesButton.Size = new System.Drawing.Size(91, 23);
+            this.BowlesButton.TabIndex = 0;
+            this.BowlesButton.Text = "Bowles Family";
+            this.BowlesButton.UseVisualStyleBackColor = true;
+            this.BowlesButton.Click += new System.EventHandler(this.BowlesButton_Click);
+            // 
+            // ThompsonButton
+            // 
+            this.ThompsonButton.Location = new System.Drawing.Point(282, 12);
+            this.ThompsonButton.Name = "ThompsonButton";
+            this.ThompsonButton.Size = new System.Drawing.Size(102, 23);
+            this.ThompsonButton.TabIndex = 1;
+            this.ThompsonButton.Text = "Thompson Family";
+            this.ThompsonButton.UseVisualStyleBackColor = true;
+            this.ThompsonButton.Click += new System.EventHandler(this.ThompsonButton_Click);
+            // 
+            // Exchanges
+            // 
+            this.Exchanges.Location = new System.Drawing.Point(234, 64);
+            this.Exchanges.Name = "Exchanges";
+            this.Exchanges.Size = new System.Drawing.Size(150, 20);
+            this.Exchanges.TabIndex = 2;
+            this.Exchanges.Visible = false;
+            // 
+            // EntryLabel
+            // 
+            this.EntryLabel.AutoSize = true;
+            this.EntryLabel.Location = new System.Drawing.Point(10, 67);
+            this.EntryLabel.Name = "EntryLabel";
+            this.EntryLabel.Size = new System.Drawing.Size(121, 13);
+            this.EntryLabel.TabIndex = 3;
+            this.EntryLabel.Text = "Enter Individual\'s Name:";
+            this.EntryLabel.Visible = false;
+            // 
+            // EntryTextBox
+            // 
+            this.EntryTextBox.Location = new System.Drawing.Point(13, 92);
+            this.EntryTextBox.Name = "EntryTextBox";
+            this.EntryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.EntryTextBox.TabIndex = 4;
+            this.EntryTextBox.Visible = false;
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.Location = new System.Drawing.Point(119, 92);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(75, 23);
+            this.EnterButton.TabIndex = 5;
+            this.EnterButton.Text = "Enter";
+            this.EnterButton.UseVisualStyleBackColor = true;
+            this.EnterButton.Visible = false;
+            // 
+            // ExchangeLabel
+            // 
+            this.ExchangeLabel.Location = new System.Drawing.Point(13, 129);
+            this.ExchangeLabel.Name = "ExchangeLabel";
+            this.ExchangeLabel.Size = new System.Drawing.Size(100, 23);
+            this.ExchangeLabel.TabIndex = 6;
+            this.ExchangeLabel.Text = "Exchange Gifts";
+            this.ExchangeLabel.UseVisualStyleBackColor = true;
+            this.ExchangeLabel.Visible = false;
+            // 
+            // FamilyExchangeTextBox
+            // 
+            this.FamilyExchangeTextBox.Location = new System.Drawing.Point(234, 191);
+            this.FamilyExchangeTextBox.Name = "FamilyExchangeTextBox";
+            this.FamilyExchangeTextBox.Size = new System.Drawing.Size(150, 20);
+            this.FamilyExchangeTextBox.TabIndex = 7;
+            this.FamilyExchangeTextBox.Visible = false;
+            // 
+            // FamilyEnterLabel
+            // 
+            this.FamilyEnterLabel.AutoSize = true;
+            this.FamilyEnterLabel.Location = new System.Drawing.Point(10, 194);
+            this.FamilyEnterLabel.Name = "FamilyEnterLabel";
+            this.FamilyEnterLabel.Size = new System.Drawing.Size(105, 13);
+            this.FamilyEnterLabel.TabIndex = 8;
+            this.FamilyEnterLabel.Text = "Enter Family\'s Name:";
+            this.FamilyEnterLabel.Visible = false;
+            // 
+            // FamilyEntryTextBox
+            // 
+            this.FamilyEntryTextBox.Location = new System.Drawing.Point(12, 221);
+            this.FamilyEntryTextBox.Name = "FamilyEntryTextBox";
+            this.FamilyEntryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.FamilyEntryTextBox.TabIndex = 9;
+            this.FamilyEntryTextBox.Visible = false;
+            // 
+            // FamilyEnterButton
+            // 
+            this.FamilyEnterButton.Location = new System.Drawing.Point(119, 219);
+            this.FamilyEnterButton.Name = "FamilyEnterButton";
+            this.FamilyEnterButton.Size = new System.Drawing.Size(75, 23);
+            this.FamilyEnterButton.TabIndex = 10;
+            this.FamilyEnterButton.Text = "Enter";
+            this.FamilyEnterButton.UseVisualStyleBackColor = true;
+            this.FamilyEnterButton.Visible = false;
+            // 
+            // FamilyExchangeButton
+            // 
+            this.FamilyExchangeButton.Location = new System.Drawing.Point(12, 257);
+            this.FamilyExchangeButton.Name = "FamilyExchangeButton";
+            this.FamilyExchangeButton.Size = new System.Drawing.Size(100, 23);
+            this.FamilyExchangeButton.TabIndex = 11;
+            this.FamilyExchangeButton.Text = "Exchange Gifts";
+            this.FamilyExchangeButton.UseVisualStyleBackColor = true;
+            this.FamilyExchangeButton.Visible = false;
+            // 
+            // ExchangeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 327);
+            this.Controls.Add(this.FamilyExchangeButton);
+            this.Controls.Add(this.FamilyEnterButton);
+            this.Controls.Add(this.FamilyEntryTextBox);
+            this.Controls.Add(this.FamilyEnterLabel);
+            this.Controls.Add(this.FamilyExchangeTextBox);
+            this.Controls.Add(this.ExchangeLabel);
+            this.Controls.Add(this.EnterButton);
+            this.Controls.Add(this.EntryTextBox);
+            this.Controls.Add(this.EntryLabel);
+            this.Controls.Add(this.Exchanges);
+            this.Controls.Add(this.ThompsonButton);
+            this.Controls.Add(this.BowlesButton);
+            this.Name = "ExchangeForm";
+            this.Text = "Exchange Gifts";
+            this.Load += new System.EventHandler(this.ExchangeForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BowlesButton;
+        private System.Windows.Forms.Button ThompsonButton;
+        private System.Windows.Forms.TextBox Exchanges;
+        private System.Windows.Forms.Label EntryLabel;
+        private System.Windows.Forms.TextBox EntryTextBox;
+        private System.Windows.Forms.Button EnterButton;
+        private System.Windows.Forms.Button ExchangeLabel;
+        private System.Windows.Forms.TextBox FamilyExchangeTextBox;
+        private System.Windows.Forms.Label FamilyEnterLabel;
+        private System.Windows.Forms.TextBox FamilyEntryTextBox;
+        private System.Windows.Forms.Button FamilyEnterButton;
+        private System.Windows.Forms.Button FamilyExchangeButton;
+    }
+}
+
---
+++ exchangeGiftsGUI/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exchangeGiftsGUI
+{
+    public partial class ExchangeForm : Form
+    {
+        public ExchangeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExchangeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BowlesButton_Click(object sender, EventArgs e)
+        {
+            EntryLabel.Visible = true;
+            EntryTextBox.Visible = true;
+            EnterButton.Visible = true;
+
+            FamilyEnterLabel.Visible = false;
+            FamilyEntryTextBox.Visible = false;
+            FamilyEnterButton.Visible = false;
+            FamilyExchangeTextBox.Visible = false;
+            FamilyExchangeButton.Visible = false;
+            Exchanges.Visible = false;
+        }
+
+        private void ThompsonButton_Click(object sender, EventArgs e)
+        {
+            EntryLabel.Visible = true;
+            EntryTextBox.Visible = true;
+            EnterButton.Visible = true;
+            FamilyEnterLabel.Visible = true;
+            FamilyEntryTextBox.Visible = true;
+            FamilyEnterButton.Visible = true;
+        }
+    }
+}
