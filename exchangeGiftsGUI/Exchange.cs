fa89549df7638ea720ddc683cfe8a9ed36db42bf
Brian <bowles123@gmail.com>
2016-04-10 14:28:13 -0600

    added class to represent exchanges

---
+++ exchangeGiftsGUI/Exchange.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Exchange
+{
+    public List<string> Names { get { return names; } }
+    public List<string> ToNames { get { return toNames; } }
+    public List<string> FromNames { get { return fromNames; } }
+    public Dictionary<string, string> LastTime { get { return lastTime; } }
+
+    private List<string> names;
+    private List<string> toNames;
+    private List<string> fromNames;
+    private Dictionary<string, string> lastTime;
+
+	public Exchange()
+	{
+        names = new List<string>();
+        toNames = new List<string>();
+        fromNames = new List<string>();
+        lastTime = new Dictionary<string, string>();
+	}
+
+    public void ShuffleNames()
+    {
+        // Variables.
+        Random rand = new Random();
+        int n = lst.Count;
+
+        // Shuffle the list.
+        while (n > 1)
+        {
+            n--;
+            int k = rand.Next(n + 1);
+            string value = Names[k];
+            Names[k] = Names[n];
+            Names[n] = value;
+        }
+    }
+
+    public bool ExchangeGift(string from, string to)
+    {
+        string name;
+        LastTime.TryGetValue(out name);
+
+        if (from != to && to != name)
+        {
+            fromNames.Add(from);
+            toNames.Add(to);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public void InitializeLastTime(string fileName)
+    {
+
+    }
+}
